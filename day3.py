44f09100c84ee455dc4bab59835d21ab9fa7dbf3
Zhannuur <zharasoovaa.zhannur.xxxx@gmail.com>
2026-05-28 10:38:07 +0500

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,25 @@
+# 1-тапсырма. Орташа мәнді есептеу бағдарламасы
+
+numbers = [10, 20, 30, 40, 50]
+
+average = sum(numbers) / len(numbers)
+
+print("Орташа мән:", average)
+
+
+# 2-тапсырма. Сандарды сұрыптау бағдарламасы
+
+numbers = [45, 12, 78, 3, 25]
+
+numbers.sort()
+
+print("Сұрыпталған тізім:", numbers)
+
+
+# 3-тапсырма. Ең кіші санды табу
+
+numbers = [14, 8, 25, 3, 19]
+
+min_number = min(numbers)
+
+print("Ең кіші сан:", min_number)
